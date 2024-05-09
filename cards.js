7cd938e5c6dfd952503b9eb8132c4804d990ac5d
Sharkota <97995086+sharkota@users.noreply.github.com>
2024-05-09 12:19:47 -0500

    LocalStorage Saving

--- cards.js
+++ cards.js
@@ -13,16 +13,16 @@ document.getElementById("theme").setAttribute("href", themeName);
 var DocCards = document.getElementById("cards");
 document.getElementById("header").textContent = settings.header
 let cards = [
-    { name: "Youtube" , link: "https://youtube.com", icon: "icon/youtube.png"},
-    { name: "Github" , link: "https://github.com", icon: "icon/github.png"}
+    { name: "Youtube", link: "https://youtube.com", icon: "icon/youtube.png" },
+    { name: "Github", link: "https://github.com", icon: "icon/github.png" }
 ];
 var NewButton = document.createElement('figure');
 NewButton.innerHTML = `+`
 NewButton.id = "AddCard"
 DocCards.appendChild(NewButton);
 const AddCardButton = document.getElementById("AddCard")
-const LoadCards = function() {
-    for(card of cards) {
+const LoadCards = function () {
+    for (card of cards) {
         AddCard(card.name, card.link, card.icon)
     };
 }
@@ -37,14 +37,25 @@ mouseY = 0
 document.addEventListener('mousemove', (event) => {
     mouseX = event.clientX;
     mouseY = event.clientY;
-  });
+});
+function HandleFile() {
+    const input = document.getElementById('file-input');
+    const file = input.files[0];
+    const reader = new FileReader();
+    reader.onload = function() {
+      const contents = reader.result;
+      localStorage.setItem("cards", contents)
+      window.location.reload()
+    };
+    reader.readAsText(file);
+  }
 function search(ele) {
-    if(event.key === 'Enter') {
+    if (event.key === 'Enter') {
         window.location.href = settings.search + ele.value
     }
 }
-const DeleteCard = function(name) {
-    for(card of cards){
+const DeleteCard = function (name) {
+    for (card of cards) {
         if (card.name == name) {
             cards.splice(cards.indexOf(card), 1)
             while (DocCards.firstChild != document.querySelector("#AddCard")) {
@@ -55,10 +66,10 @@ const DeleteCard = function(name) {
         }
     }
 }
-const DeletePopUp = function() {
+const DeletePopUp = function () {
     document.querySelector(".popup").remove()
 }
-const EditIcon = function(name) {
+const EditIcon = function (name) {
     icon = prompt("Icon Link")
     if (card.name == name) {
         let indexnum = cards.indexOf(card)
@@ -70,27 +81,28 @@ const EditIcon = function(name) {
         LoadCards()
     }
 }
-const SetSettings = function(setting) {
+const SetSettings = function (setting) {
     console.log(setting)
     const newValue = prompt(`New Value for ${setting}`);
     settings[setting] = newValue;
     localStorage.setItem("settings", JSON.stringify(settings))
     window.location.reload()
 }
-const OpenSettings = function(){
+const OpenSettings = function () {
     var PopUp = document.createElement('div')
     PopUp.classList.add("popup")
     PopUp.style.left = '94vw'
     PopUp.style.top = '1vh'
-    PopUp.innerHTML = `<ul><li><button onclick="SetSettings('theme')">Theme</button></li><li><button onclick="SetSettings('header')">Home</button></li><li><button onclick="SetSettings('search')">Search URL</button></li><li><button style="color: red;" onclick="localStorage.clear();window.location.reload()">DELETE ALL DATA</button></li></ul>`
+    PopUp.innerHTML = `<ul><li><button onclick="SetSettings('theme')">Theme</button></li><li><button onclick="SetSettings('header')">Home</button></li><li><button onclick="SetSettings('search')">Search URL</button></li><li><button onclick="document.getElementById('file-input').click();">Import Cards</button><input id="file-input" type="file" name="name" style="display: none;"/></li><li><button onclick="ExportLocal('cards');">Export Cards</button><li><button style="color: red;" onclick="localStorage.clear();window.location.reload()">DELETE ALL DATA</button></li></ul>`
     document.body.appendChild(PopUp)
+    document.querySelector("#file-input").addEventListener("input", HandleFile);
 }
-const CardConfig = function(name) {
+const CardConfig = function (name) {
     StringName = "'" + name + "'"
     var PopUp = document.createElement('div')
     PopUp.classList.add("popup")
-    PopUp.style.left = mouseX+'px'
-    PopUp.style.top = mouseY+'px'
+    PopUp.style.left = mouseX + 'px'
+    PopUp.style.top = mouseY + 'px'
     PopUp.innerHTML = `<ul><li><button onclick="DeleteCard(${StringName});DeletePopUp()">Delete</button></li><li><button onclick="EditIcon(${StringName});DeletePopUp()">Edit Icon</button></li></ul>`
     document.body.appendChild(PopUp)
 }
@@ -98,7 +110,7 @@ function AddCard(name, link, icon) {
     StringName = "'" + name + "'"
     link = link + ''
     if (icon == "icon" | icon == undefined) {
-    icon = "https://icons.duckduckgo.com/ip3/" + link.split("https://")[1] + ".ico"
+        icon = "https://icons.duckduckgo.com/ip3/" + link.split("https://")[1] + ".ico"
     }
     var NewCard = document.createElement('figure');
     NewCard.id = name;
@@ -107,11 +119,39 @@ function AddCard(name, link, icon) {
     DocCards.appendChild(NewCard);
     NewCard.parentNode.insertBefore(NewCard, AddCardButton)
 };
+function Get_Local(key) {
+    return localStorage.getItem(key);
+}
+function New_Blob(contents) {
+    return new Blob([contents], { type: 'text/plain' });
+}
+function Blob_DL(blob, fileName) {
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = fileName;
+    link.style.display = 'none';
+    document.body.appendChild(link);
+    link.click();
+    URL.revokeObjectURL(url);
+    document.body.removeChild(link);
+}
+function ExportLocal(key) {
+    const key_contents = Get_Local(key);
+    const blob = New_Blob(key_contents);
+    const fileName = 'clean-tab.' + key + '.json';
+    Blob_DL(blob, fileName);
+}
+function ImportLocal(key) {
+    const fileInput = document.createElement('input');
+    fileInput.type = 'file';
+    fileInput.accept = '.json';
+}
 // ACTIONS
-AddCardButton.addEventListener("click", function() {
+AddCardButton.addEventListener("click", function () {
     var name = prompt("What is the name of the card?")
     var link = prompt("What is the link of the card?")
-    if(link.endsWith("/")) {
+    if (link.endsWith("/")) {
         link = link.slice(0, -1)
     }
     AddCard(name, link)
